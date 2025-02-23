7f575d0c6b7f715eb75bdac18db7ce300d24187f
Maor Meir Hajaj <maor@hajaj-projects.com>
2025-02-23 14:19:00 +0000

    add dev con config - add gitignore

--- app.py
+++ app.py
@@ -18,14 +18,17 @@ load_dotenv()
 
 HUGGINGFACE_API_KEY = os.environ['HUGGINGFACE_API_KEY']
 
+
 def process_input(input_type, input_data):
     """Processes different input types and returns a vectorstore."""
     documents = ""
     if input_type == "Link":
         loader = WebBaseLoader(input_data)
         web_documents = loader.load()
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-        texts = [str(doc.page_content) for doc in text_splitter.split_documents(web_documents)]
+        text_splitter = CharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=100)
+        texts = [str(doc.page_content)
+                 for doc in text_splitter.split_documents(web_documents)]
     elif input_type == "PDF":
         for file in input_data:
             pdf_reader = PdfReader(BytesIO(file.read()))
@@ -44,7 +47,8 @@ def process_input(input_type, input_data):
         raise ValueError("Unsupported input type")
 
     if input_type != "Link":
-        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+        text_splitter = CharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=100)
         texts = text_splitter.split_text(documents)
 
     model_name = "sentence-transformers/all-mpnet-base-v2"
@@ -70,37 +74,45 @@ def process_input(input_type, input_data):
     vector_store.add_texts(texts)  # Add documents to the vector store
     return vector_store
 
+
 def answer_question(vectorstore, query):
     """Answers a question based on the provided vectorstore."""
     try:
         llm = HuggingFaceEndpoint(
-            repo_id='mistralai/Mistral-7B-Instruct-v0.2',
+            repo_id='mistralai/Mistral-7B-Instruct-v0.3',
             token=HUGGINGFACE_API_KEY,
             temperature=0.6,
-            max_new_tokens=1000
+            max_new_tokens=4000
         )
-        qa = RetrievalQA.from_chain_type(llm=llm, retriever=vectorstore.as_retriever())
+        qa = RetrievalQA.from_chain_type(
+            llm=llm, retriever=vectorstore.as_retriever())
         result = qa({"query": query})
         return result.get("result", "No answer found. The model might not have understood the question.")
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
+
 def main():
     st.title("AskIt - Q&A with Multiple Files")
-    st.write("Upload documents or provide a link to ask questions based on their content.")
+    st.write(
+        "Upload documents or provide a link to ask questions based on their content.")
 
-    input_type = st.selectbox("Input Type", ["Link", "PDF", "Text", "DOCX", "TXT"])
+    input_type = st.selectbox(
+        "Input Type", ["Link", "PDF", "Text", "DOCX", "TXT"])
     input_data = None
 
     if input_type == "Link":
-        number_of_links = st.number_input("Number of Links", min_value=1, max_value=20, step=1)
-        input_data = [st.text_input(f"Link {i+1}") for i in range(int(number_of_links))]
-        input_data = [link for link in input_data if link.strip()]  # Remove empty links
+        number_of_links = st.number_input(
+            "Number of Links", min_value=1, max_value=20, step=1)
+        input_data = [st.text_input(f"Link {i+1}")
+                      for i in range(int(number_of_links))]
+        # Remove empty links
+        input_data = [link for link in input_data if link.strip()]
     elif input_type == "Text":
         input_data = st.text_area("Enter the text")
     elif input_type in ["PDF", "DOCX", "TXT"]:
         input_data = st.file_uploader(
-            f"Upload {input_type} files", type=[input_type.lower()], accept_multiple_files=True
+            f"Upload {input_type} files", type=["*"], accept_multiple_files=True
         )
 
     if st.button("Process Input"):
@@ -114,13 +126,15 @@ def main():
                 st.session_state["vectorstore"] = vectorstore
                 st.success("Input processed successfully!")
             except Exception as e:
-                st.error(f"An error occurred while processing the input: {str(e)}")
+                st.error(
+                    f"An error occurred while processing the input: {str(e)}")
 
     if "vectorstore" in st.session_state:
         query = st.text_input("Ask your question:")
         if st.button("Submit"):
             if query.strip():
-                answer = answer_question(st.session_state["vectorstore"], query)
+                answer = answer_question(
+                    st.session_state["vectorstore"], query)
                 st.markdown(f"**Answer:** {answer}")
                 if "history" not in st.session_state:
                     st.session_state["history"] = []
@@ -134,6 +148,7 @@ def main():
                 for i, (q, a) in enumerate(st.session_state["history"], 1):
                     st.markdown(f"**Q{i}:** {q}")
                     st.markdown(f"**A{i}:** {a}")
+
 
 if __name__ == "__main__":
     main()
